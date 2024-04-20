43ad4debf3a80f1514af8a9103f1d38a4d15bb27
arpitnuwal <arpitnuwal1996@gmail.com>
2024-04-20 13:34:19 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const ejs = require('ejs');
 const app = express();
-
+const path = require('path');
 // Loading environment variables
 require('dotenv').config();
 
 // BodyParser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 // Define port from environment variables with a fallback
 const port = process.env.PORT || 3000;
 
@@ -19,6 +20,7 @@ const dbURI = process.env.MONGODB_URI;
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
+
 })
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
@@ -34,6 +36,7 @@ const Register = mongoose.model("Register", registerSchema);
 
 // Routes
 app.get("/", (req, res) => {
+  console.log(dbURI);
   res.sendFile(__dirname + "/pages/index.html");
 });
 
@@ -53,11 +56,17 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.get("/success", (req, res) => {
+app.get("/success",async (req, res) => {
+  const registers = await Register.find();
+ res.render('success', { registers: registers });
   res.sendFile(__dirname + "/pages/success.html");
 });
 
-app.get("/error", (req, res) => {
+app.get("/error",async (req, res) => {
+
+  const registers = await Register.find();
+  res.json(registers);
+  console.log(registers);
   res.sendFile(__dirname + "/pages/error.html");
 });
 
